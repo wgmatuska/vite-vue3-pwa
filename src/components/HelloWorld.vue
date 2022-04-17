a42e21782994e4e0e1ba2cb7c65c34b309f3e929
W. T. Moose <wtmoose@thingconnect.cc>
2022-04-17 16:44:05 -0400

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+  secondmsg: String,
+  thirdmsg: String
+})
+
+const count = ref(0)
+</script>
+
+
+
+<template>
+  <p>{{ msg }}</p>
+
+  <p>{{ secondmsg }}</p>
+
+  <p>
+    Just some text in a paragraph.
+  </p>
+
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>{{ thirdmsg }}</p>
+</template>
+
+
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
